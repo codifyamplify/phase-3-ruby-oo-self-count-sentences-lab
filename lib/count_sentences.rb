9e56f2cefd44ba7e02c74c5e645018766c8c6948
David Whitney <codifyamplify@gmail.com>
2022-12-24 16:11:38 -0500

    commit tree 4f038bfc4a86ab01565ec4a360b7ebc85e339b29

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,35 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    # start with self
+    # next use .split() and figure out how to use regula expression to fill arguments
 
+    # last use .length or .size to get a count of elements
+    self.split(/\.|\?|!/).filter {|sentence| !sentence.empty? } .length
   end
 end
+
+# puts "The weather outside is frightful.".sentence?
+# returned true!
+
+# puts "Where am I?".question?
+# true!
+# puts "I am where you are.".question?
+# false!
+
+# puts "I'm old now!".exclamation?
+# true
+
+# pp "I'm. Not!! Here?".count_sentences
